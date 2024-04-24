df94d2f2499f55aa8952f69df93799aea05108fb
yeferson castiblanco <y.castiblanco@inlaze.com>
2024-04-24 16:21:04 -0500

    changes load_data

--- load_models.py
+++ load_models.py
@@ -1,5 +1,7 @@
 from data.database import Base
 from data.database import get_engine
+
+from app.domain.models_sql import User
 def load_models():
     engine = get_engine()
     Base.metadata.create_all(bind=engine)
